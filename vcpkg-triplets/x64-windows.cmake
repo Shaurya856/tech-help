d41b243c62771889fd8fe348add87531dacbab5b
Shaurya856 <malooshaurya@gmail.com>
2026-06-22 13:36:02 +0530

    fix v3 build

--- vcpkg-triplets/x64-windows.cmake
+++ vcpkg-triplets/x64-windows.cmake
@@ -1,3 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+# We only ship Release builds - skip building each dependency's debug
+# config too, which otherwise roughly doubles dependency build time.
+set(VCPKG_BUILD_TYPE release)
